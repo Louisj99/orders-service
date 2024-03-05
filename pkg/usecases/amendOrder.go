3f6bd3c94818d3fef2cc80f4618c065e8e9a6d03
louisj99 <louis.stockton@hotmail.co.uk>
2024-03-05 11:21:31 +0000

    created amend oder types and interface

--- pkg/usecases/amendOrder.go
+++ pkg/usecases/amendOrder.go
@@ -1,1 +1,23 @@
 package usecases
+
+import (
+	"context"
+	"orders-service/pkg/entities"
+)
+
+type AmendOrderRequest struct {
+	Status       string  `json:"status" binding:"required"`
+	ItemID       string  `json:"itemID" binding:"required" `
+	OrderID      string  `json:"orderID" binding:"required" `
+	Quantity     int     `json:"quantity" binding:"required"`
+	PricePerUnit float64 `json:"pricePerUnit" binding:"required" `
+}
+
+type AmendOrderResponse struct {
+	Order      entities.Order       `json:"order"`
+	OrderItems []entities.OrderItem `json:"orderItem"`
+}
+
+type AmendOrderRepository interface {
+	AmendOrder(context.Context, entities.Order) error
+}
